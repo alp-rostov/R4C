d9f654ae123db9ed31d4893c3118d590831ca287
alprostov <alp-rostov@mail.ru>
2024-12-13 20:11:34 +0400

    add number generation

--- robots/views.py
+++ robots/views.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from django.http import JsonResponse
 from django.views.generic import CreateView, TemplateView
 from robots.forms import RobotForm
@@ -6,8 +5,14 @@ from robots.forms import RobotForm
 
 class CreateRobot(TemplateView):
     template_name = 'robot/createrobot.html'
+
     def post(self, request, *args, **kwargs):
-        form = RobotForm(self.request.POST)
+        model = self.request.POST.get('model')
+        version = self.request.POST.get('version')
+        serial_number = f'{model}-{version}'
+        copy_request = self.request.POST.copy()
+        copy_request.appendlist('serial',serial_number)
+        form = RobotForm(copy_request)
         if form.is_valid():
             form.save()
             return JsonResponse({'message': 'Робот добавлен'})
